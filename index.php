9e3a1b92d5315e8fae8b311e9a4c19d746510225
Anubhav-sar <anubhavsarswat320@gmail.com>
2024-07-04 21:18:30 +0530

    login page design index page design new css page for all seprate page

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
     <link rel="stylesheet" href="css/indexstyle.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="main">
     <h1>Welcome to School Management system</h1>
     <div class= "button_container">
-        <button class="button-56"><a href="#">Student Result</a></button>
-        <button class="button-56"><a href="#">Admin</a></button>
+        <button class="button-56"><a href="result.php">Student Result</a></button>
+        <button class="button-56"><a href="login.php">Admin</a></button>
     </div> 
     </div>
 </body>
---
+++ result.php
@@ -0,0 +1,25 @@
+<php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/result.css">
+</head>
+<body>
+<table>
+  <thead>
+    <th colspan="2">Student Information</th>
+  </thead>
+  <tr>
+    <td>Class</td>
+    <td>Student Roll Num</td>
+  </tr>
+</table>
+
+</body>
+</html>
